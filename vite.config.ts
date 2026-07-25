5df080720588324a3c3fd3fbc7ed08bcfbe2a4e7
Guanais414 <joaopedroguanais@gmail.com>
2026-07-25 16:39:48 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -24,4 +25,11 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}); 
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', 
+
